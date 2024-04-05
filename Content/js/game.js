2fee00467753f4d830c73359094e37dff8975266
tleung <ahkhiat@hotmail.com>
2024-04-05 10:41:38 +0200

    js presque good

--- Content/js/game.js
+++ Content/js/game.js
@@ -6,83 +6,105 @@ let gameContainer = document.getElementById("game_container");
 
     if(gameContainer)
     {
-        console.log("toto")
+        console.log("game container loaded")
 
         let questions;
+        let questionsArray;
 
-        getListeQuestions();
-        async function getListeQuestions() {
-            const resType = await fetch("?controller=game&action=fetch_questions", {
+        async function fetchQuestions() {
+            const res = await fetch("?controller=game&action=fetch_questions", {
                 method: 'GET' ,
                 headers: {
                     'Accept': 'application/json, text/plain, */*',
                     'Content-type': 'application/json'
                 } 
             });
-            
-            questions = await resType.json();
-            console.log(questions);
+            questionsArray = await res.json();
+            questions = questionsArray.game
+            // console.log(questions);
+            }
+        async function getListeQuestions() {
+            await fetchQuestions();
+        }
+
+        getListeQuestions().then(() => {
+            console.log('question test : ', questions);
+
+            let question = document.querySelector("#question");
+            let reponses = document.querySelector("#answers");
+            let questionActuelle = 0;
+            let score = 0;
+            let bonneReponseIndex = 0;
+            let bonneReponse = questions[questionActuelle].answers[bonneReponseIndex];
+
+
+            genererQuestion()
+
+            function genererQuestion() {
+                question.innerText = questions[questionActuelle].question;
+                bonneReponse = questions[questionActuelle].answers[bonneReponseIndex];
+                    for (let i=0; i < questions[questionActuelle].answers.length; i++) {
+                        let reponse = document.createElement("li");
+                        reponse.classList.add("answer");
+                        reponse.innerText = questions[questionActuelle].answers[i]
+                        reponses.appendChild(reponse)
+                    }
+                console.log(bonneReponse)
             }
 
-        let question = document.querySelector("#question");
-        let reponses = document.querySelector("#answers");
-        let questionActuelle = 0;
-        let score = 0;
-        let bonneReponseIndex = questions[questionActuelle].correctAnswerIndex;
-        let bonneReponse = questions[questionActuelle].answers[bonneReponseIndex];
-
-        genererQuestion();
-
-        function genererQuestion() {
-            question.innerText = questions[questionActuelle].question;
-            bonneReponseIndex = questions[questionActuelle].correctAnswerIndex;
-            bonneReponse = questions[questionActuelle].answers[bonneReponseIndex];
-                for (let i=0; i < questions[questionActuelle].answers.length; i++) {
-                    // console.log(questions[questionActuelle].answers[i])
-                    let reponse = document.createElement("li");
-                    reponse.classList.add("answer");
-                    reponse.innerText = questions[questionActuelle].answers[i]
-                    reponses.appendChild(reponse)
+            function afficherScore() {
+                let cadreScore = document.querySelector("#score")
+                cadreScore.innerText = score
+            }
+    
+            function questionSuivante () {
+                reponses.innerHTML = "";
+    
+                if (questionActuelle < questions.length - 1) {
+                        questionActuelle++;
+                        genererQuestion();
+                        afficherScore();
+                    } else {
+                        question.innerText = "";
+                        question.innerHTML = `Merci d'avoir participé à ce quiz, votre score est de ${score} bonnes réponses sur 4 !`
+                        reponses.remove();
+                        afficherScore();
                 }
-            console.log("Index Bonne reponse", bonneReponseIndex)
-            console.log(bonneReponse)
-        }
+            }
 
-        function afficherScore() {
-            let cadreScore = document.querySelector("#score")
-            cadreScore.innerText = score
-        }
-
-        function questionSuivante () {
-            reponses.innerHTML = "";
-
-            if (questionActuelle < questions.length - 1) {
-                    questionActuelle++;
-                    genererQuestion();
-                    afficherScore();
+            reponses.addEventListener("click", (event) => {
+                let reponseChoisie = event.target.innerText
+                console.log('reponse choisie', reponseChoisie)
+    
+                if (reponseChoisie == bonneReponse) {
+                    console.log("c'est gagné")
+                    score++;
+                    console.log("score", score)
+                    console.log("question actuelle", questionActuelle)
+                    questionSuivante()
                 } else {
-                    question.innerText = "";
-                    question.innerHTML = `Merci d'avoir participé à ce quiz, votre score est de ${score} bonnes réponses sur 4 !`
-                    reponses.remove();
-                    afficherScore();
-            }
-        }
+                    questionSuivante()
+                }
+            })
 
 
-        reponses.addEventListener("click", (event) => {
-            let reponseChoisie = event.target.innerText
-            console.log(reponseChoisie)
 
-            if (reponseChoisie == bonneReponse) {
-                console.log("c'est gagné")
-                score++;
-                console.log("score", score)
-                console.log("question actuelle", questionActuelle)
-                questionSuivante()
-            } else {
-                questionSuivante()
-            }
-        })
+        });
+
+
+        
+        // let bonneReponseIndex = questions[questionActuelle].correctAnswerIndex;
+        // let bonneReponse = questions[questionActuelle].answers[bonneReponseIndex];
+
+
+        // genererQuestion();
+
+        
+
+        
+
+
+        
   
 
 
