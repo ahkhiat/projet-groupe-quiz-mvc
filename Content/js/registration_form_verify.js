62e3931ecbd295ac722f36639fc99e9e9adff595
tleung <ahkhiat@hotmail.com>
2024-04-15 17:02:01 +0200

    verifs password js en cours

--- Content/js/registration_form_verify.js
+++ Content/js/registration_form_verify.js
@@ -5,29 +5,50 @@ document.addEventListener("DOMContentLoaded", () => {
     if(registrationContainer)
     {
         console.log('registration_form_verify loaded');
-        // ..............verification du mot de passe avant enregistrement
+
+        const uppercaseRegex = /[A-Z]/;
+        const lowercaseRegex = /[a-z]/;
+        const specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
         const registrationForm = document.getElementById('registration-form');
-        let password = document.querySelector('input[name="password"]').value;
+        // let password = document.querySelector('input[name="password"]').value;
+        let errorContainer = document.getElementById('error-message');
 
+        // EN COURS 
+        document.querySelector('input[name="password"]').addEventListener('input', (event) => {
+            let password = event.target.value;
+
+            if(uppercaseRegex.test(password)) {
+                console.log('Il y a une majuscule');
+                errorContainer.innerText += 'Il y a bien une majuscule'
+            }
+            if(lowercaseRegex.test(password)) {
+                console.log('Il y a une minuscule');
+                errorContainer.innerText += 'Il y a bien une minuscule'
+
+            }
+            if(specialCharRegex.test(password)) {
+                console.log('Il y a un caractère spécial');
+                errorContainer.innerText += 'Il y a bien un caractère spécial'
+
+            }
+
+        })
+    
         registrationForm.addEventListener('submit', function(event) {
 
             password = document.querySelector('input[name="password"]').value;
-
-            const uppercaseRegex = /[A-Z]/;
-            const lowercaseRegex = /[a-z]/;
-            const specialCharRegex = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
             //........... Vérifier si le mot de passe a moins de 11 caractères
             if (password.length < 11) {
                 console.log('le mot de passe est inférier à 11');
                 event.preventDefault(); //.....................Empêcher l'envoi du formulaire
-                document.getElementById('error-message').textContent = 'Le mot de passe doit avoir au moins 11 caractères.';
+                errorContainer.textContent = 'Le mot de passe doit avoir au moins 11 caractères.';
             } else if (!uppercaseRegex.test(password) || !lowercaseRegex.test(password) || !specialCharRegex.test(password)) {
                 event.preventDefault(); //...........Empêcher l'envoi du formulaire
-                document.getElementById('error-message').textContent = 'Le mot de passe doit contenir au moins une majuscule, une minuscule et un caractère spécial.';
+                errorContainer.textContent = 'Le mot de passe doit contenir au moins une majuscule, une minuscule et un caractère spécial.';
             } else {
-                document.getElementById('error-message').textContent = ''; //.................Effacer le message d'erreur s'il existe
+                errorContainer.textContent = ''; //.................Effacer le message d'erreur s'il existe
             }
         });
 
