fcf5f5fc5babfa1c5a23779338a180e2f022290d
nadia <anabelle0025@yahoo.fr>
2024-04-16 09:42:28 +0200

    validator password

--- Views/Security/view_user_registration.php
+++ Views/Security/view_user_registration.php
@@ -64,19 +64,25 @@
                 <span class="input-group-text"><i class="fa fa-envelope"></i></span>   
                 <input  type="text" class="form-control" name="email" placeholder="Entrer votre email" required>    
               </div>
-              <div class="input-group mb-3">
+              <div class=" block-input input-group mb-3">
                 <span class="input-group-text"><i class="fa fa-lock"></i></span>  
                 <input  type="password"id='current-password' class="form-control" name="password" placeholder="Entrer votre mot de passe" required>
                 <button type="button" id="button" class="btn btn-outline-secondary"><i class="fa fa-eye"></i></button>    
               </div>
-            
-            
+          
               <div class="d-grid">
                 <div id="error-message" class="text-danger">
                 </div>
                 <button type="submit" name="submit_registration" class="btn btn-primary">Envoyer</button>
-            
               </div>
+          <div class="validator-criters">
+            <span class="generique"><i class="far fa-check-circle"></i> &nbsp;Votre mot doit comporter 11 Caractères au minimum</span>
+            <span class="majuscule"><i class="far fa-check-circle"></i> &nbsp;Votre mot de passe doit avoir 1 lettre majuscule</span>
+            <span class="minuscule"><i class="far fa-check-circle"></i> &nbsp;Votre mot de passe doit avoir 1 lettre minuscule</span>
+            <span class="caractere"><i class="far fa-check-circle"></i> &nbsp;Votre mot de passe doit avoir 1 caracter spécial</span>
+            <span class="chiffre"><i class="far fa-check-circle"></i> &nbsp;Votre mot de passe doit avoir 1 chiffre</span>
+      
+        </div>
         
       </form>
   </div>
